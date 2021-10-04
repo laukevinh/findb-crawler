a251d1aaa091cdcd4c301f4239f3498b6dcd499a
klau <laudevelopment@gmail.com>
2021-10-04 03:59:01 -0500

    Show last crawled status

--- front-end/src/components/HouseZipUrl.js
+++ front-end/src/components/HouseZipUrl.js
@@ -5,7 +5,7 @@ function HouseZipUrl(props) {
 
   return (
     <div className="text-center" key={index}>
-      {year}: {url} ({crawled_on ? "N/A" : crawled_on})
+      {year}: {url} ({crawled_on === undefined ? "N/A" : crawled_on})
     </div>
   );
 }
