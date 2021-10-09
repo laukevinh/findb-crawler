5733d7028540a2515780a816517c442c7753b472
klau <laudevelopment@gmail.com>
2021-10-09 18:22:46 -0500

    Organize app.js content into Semantic containers

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import HouseZipUrlContainer from './components/HouseZipUrlContainer';
-import { API_URL, API_YEAR_URL } from './constants';
 import { useState } from 'react';
 import HouseFdContainer from './components/HouseFdContainer';
+import { Container, Divider, Grid, Header } from 'semantic-ui-react';
 
 function App() {
   const [houseFdData, setHouseFdData] = useState([]);
 
   return (
     <div className="App">
-      <h2>House Financial Disclosures Zip Urls</h2>
+      <Container>
+        <Header as='h1' dividing>
+          House Financial Disclosures Zip Urls
+        </Header>
+      </Container>
       <HouseZipUrlContainer
         houseFdData={houseFdData}
         setHouseFdData={setHouseFdData}
       />
-      <br />
-      <br />
-      <h2>Transactions</h2>
+      <Divider hidden />
       <HouseFdContainer houseFdData={houseFdData} />
+
     </div>
   );
 }
